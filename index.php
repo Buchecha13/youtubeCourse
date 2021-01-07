d497562066175258c49b775b5606411dc5a5ada0
BovgiraV <BovgiraV@yandex.ru>
2021-01-07 13:13:34 +0700

    string functions

--- index.php
+++ index.php
@@ -1,35 +1,5 @@
 <?php
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$text = $_POST['text'];
+$str = 'Hello Маша';
 
-$app = $name . '|' . $email . '|' . $text;
-
-var_dump($app);
-
-file_put_contents('apps.txt', $app, FILE_APPEND);
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-
-    <form method="post" action="">
-        <input type="text" name="name"><br>
-        <input type="email" name="email"><br>
-        <textarea name="text" cols="40" rows="3"></textarea><br>
-        <button type="submit">Отправить</button>
-    </form>
-
-</body>
-
-</html>
+var_dump(mb_substr($str, 6, 4));
