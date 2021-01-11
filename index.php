775d2da119d11fe5748935012e2e55b217795b0d
BovgiraV <BovgiraV@yandex.ru>
2021-01-11 20:36:47 +0700

    delete all files

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 
-$str = 'Hello Маша';
+class User
+{
+    public $sql;
 
-var_dump(mb_substr($str, 6, 4));
+    public function addUser(string $login, string $password)
+    {
+        if (strlen($login) > 12 || strlen($login) < 3) {
+            return false;
+        }
+        if (strlen($login) > 6 || strlen($login) < 4) {
+            return false;
+        }
+        $this->sql = "INSERT INTO users VALUES('', {$login}, {$password})";
+
+        return true;
+    }
+}
+
+$user = new User();
+$result = $user->addUser('Misha', '1234');
+echo $user->sql;
+echo '<br>';
+echo $result;
